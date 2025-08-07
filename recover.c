86ee9843c46843c19d56be7ba7c9fd25514e2497
AnshikaBisht1202 <anshikabisht1202@gmail.com>
2025-08-07 23:10:13 +0530

    Add files via upload

---
+++ recover.c
@@ -0,0 +1,50 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    // Accept a single command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    // Open the memory card
+    FILE *card = fopen(argv[1], "r");
+
+    // Create a buffer for a block of data
+    uint8_t buffer[512];
+
+    int file_no = 0;
+    FILE *img = NULL;
+    char filename[8];
+
+    // While there's still data left to read from the memory card
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        // Create JPEGs from the data
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0)
+        {
+            if (img != NULL) // if img initially isn't empty, close
+            {
+                fclose(img);
+            }
+            sprintf(filename, "%03i.jpg", file_no); // new jpeg file (001,002, and so on)
+            img = fopen(filename, "w");
+            file_no++; // helps with 001,002,003 kind naming
+        }
+        if (img != NULL)
+        {
+            fwrite(buffer, 1, 512, img);
+        }
+    }
+    if (img != NULL)
+    {
+        fclose(img);
+    }
+    fclose(card);
+    return 0;
+}
